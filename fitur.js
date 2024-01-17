ff20127c481a22bca4707e1c1a2f17ef1d8751e2
Ryandra-TI01 <ryandra.athaya1705@gmail.com>
2024-01-17 21:39:52 +0700

    done 100%

--- fitur.js
+++
@@ -1,9 +0,0 @@
-function tampilkan_judul() {
-
-    document.getElementById("judul_3").style.display ="block";
-    document.getElementById("judul_1").style.display ="none";
-  }
-function tampilkan_judul1() {
-    document.getElementById("judul_3").style.display ="none";
-    document.getElementById("judul_1").style.display ="block";
-  }
